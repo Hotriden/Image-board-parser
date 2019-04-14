48b030981a03e506984756e09f0fd79b94e4edb0
Hotriden <Bloodridetv@gmail.com>
2019-04-14 12:20:29 +0300

    home_commite

---
+++ WPFTest/MainParser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AngleSharp.Html.Parser;
+using System.Net;
+using System.Threading;
+
+namespace WPFTest
+{
+    public class MainParser
+    {
+        public List<string> ParseUrlByAngle(int number, string section)
+        {
+            string firstSymb = "https://2ch.hk/";
+            string url = firstSymb + section + "/res/" + number + ".html";
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string Response = wc.DownloadString(url);
+            var parser = new HtmlParser();
+            var search = parser.ParseDocument(Response);
+            var result = search.GetElementsByClassName("post__file-attr").ToArray();
+
+            List<string> FinalResult = new List<string>();
+
+                foreach (var b in result)
+                {
+                    string pattern = "href='".Replace("'", "\"") + "/" + section;
+                    string[] splitted = b.InnerHtml.Split();
+                    string temp = null;
+                    for (int i = 0; i < splitted.Length; i++)
+                    {
+                        if (splitted[i].StartsWith(pattern))
+                        {
+                            temp += splitted[i];
+                        }
+                    }
+
+                    string subString = @"href=""";
+                    int indexOfSubString = temp.IndexOf(subString);
+                    string final = temp.Remove(indexOfSubString, subString.Length);
+                    string finalResult = firstSymb + final.Remove(final.Length - 1, 1);
+                    FinalResult.Add(finalResult);
+                }
+            
+            return FinalResult;
+
+        }
+
+        public void SaveImage(string imageUrl, string pathFile)
+        {
+            string reversedUrl;
+            string backforward = null;
+            char[] revereUrl = imageUrl.ToCharArray();
+            Array.Reverse(revereUrl);
+            reversedUrl = new string(revereUrl);
+
+            for (int i = 0; i<reversedUrl.Length; i++)
+            {
+                if (reversedUrl[i] == '/')
+                {
+                    break;
+                }
+                else
+                {
+                    backforward += reversedUrl[i];
+                }
+            }
+            char[] secondRevers = backforward.ToCharArray();
+            Array.Reverse(secondRevers);
+            reversedUrl = new string(secondRevers);
+            string fileName = pathFile + @"\" + reversedUrl;
+            
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.DownloadFileAsync(new Uri(imageUrl), fileName);
+            }
+        }
+    }
+}
---
+++ WPFTest/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AngleSharp.Html.Parser;
+using Microsoft.Win32;
+using System.IO;
+using System.Net;
+using System.ComponentModel;
+using System.Threading;
+
+namespace WPFTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainParser pars = new MainParser();
+        WebClient client = new WebClient();
+        List<string> links = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Parsed_OnNewData(object obj, string[] obj2)
+        {
+
+        }
+
+        private void Parsed_OnCompleted(object obj)
+        {
+
+        }
+
+        private void BtnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxFind.Text != "")
+            {
+                try
+                {
+                    List<string> result = pars.ParseUrlByAngle(int.Parse(TextBoxFind.Text), TextBoxSection.Text);
+
+                    foreach (var b in result)
+                    {
+                        links.Add(b);
+                        TextBox textbox = new TextBox();
+                        textbox.Text = b;
+                        textbox.HorizontalAlignment = HorizontalAlignment.Left;
+                        textbox.Width = 300;
+                        textbox.IsReadOnly = true;
+                        textbox.Margin = new Thickness(0, 0, 10, 0);
+
+                        myStackPanel.Children.Add(textbox);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("В процессе работы возникла ошибка: " + ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не корректный ввод данных. Проверьте правильность заполнения.");
+            }
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+                if (TextBoxFilePath.Text != "")
+                {
+                    try
+                    {
+                        foreach (var b in links)
+                        {
+                            pars.SaveImage(b, TextBoxFilePath.Text);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("В процессе работы возникла ошибка: " + ex);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите путь для сохранения файлов");
+                }
+        }
+
+        private void BtnLoad_Select(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxFilePath.Text != null)
+            {
+
+                System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+
+                System.Windows.Forms.DialogResult dlg = fbd.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    TextBoxFilePath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+    }
+}
